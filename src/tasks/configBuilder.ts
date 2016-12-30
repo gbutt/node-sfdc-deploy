fe1a5b670a5a407b5780dfe354b154882be9301a
Greg Butt <gbutt@rallydev.com>
2016-12-30 14:18:10 -0700

    default config

--- src/tasks/configBuilder.ts
+++ src/tasks/configBuilder.ts
@@ -1,15 +1,15 @@
-import {readFileSync} from 'fs';
+import * as fs from 'fs';
 import {Config} from '../config';
 
 export function buildAddonConfiguration(options: Config): Config {
-  const pkg = JSON.parse(readFileSync('package.json').toString());
+  const pkg = JSON.parse(fs.readFileSync('package.json').toString());
   let config = {
     resourceName: pkg.name,
     resourceDescription: pkg.description,
     deployDir: 'deploy'
   } as Config;
-  if (options && options.configFile) {
-    var configFile = JSON.parse(readFileSync(options.configFile).toString());
+  if ((options && options.configFile) || fs.existsSync('sfdcDeploy.json')) {
+    var configFile = JSON.parse(fs.readFileSync(options.configFile).toString());
     Object.assign(config, configFile);
   }
   Object.assign(config, options);
